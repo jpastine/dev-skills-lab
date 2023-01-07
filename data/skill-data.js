4252a621d6bab71674faa0bd6328d18933976ca9
justinpastine <justin.pastine@gmail.com>
2023-01-07 16:33:46 -0500

    add router and refactor

---
+++ data/skill-data.js
@@ -0,0 +1,12 @@
+const skills = [
+  {text: 'HTML', done: false,},
+  {text: 'CSS', done: false}, 
+  {text: 'JavaScript', done: false},
+  {text: 'MongoDB', done: false},
+  {text: 'Node', done: false},
+  {text: 'Express', done: false}
+]
+
+export {
+  skills
+}
