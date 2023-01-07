aa7469d9df02af7e8c00cff33257a0eb6ff86900
justinpastine <justin.pastine@gmail.com>
2023-01-07 17:22:56 -0500

    add create function

--- controllers/skills.js
+++ controllers/skills.js
@@ -17,7 +17,21 @@ function newSkill(req, res) {
   res.render('skills/new')
 }
 
+function create(req, res) {
+  console.log(req.body)
+  req.body.done = false
+  Skill.create(req.body)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
 export{
   index,
   newSkill as new,
+  create,
 }
